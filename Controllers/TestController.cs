9202d58831946fbb971510c2de9013d208867823
London-Cyber020 <londoncyber020@gmail.com>
2026-04-13 03:33:17 +0300

    Create TestController with ping and status methods
    Added TestController with ping and status endpoints.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProfitX.CloudServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet("ping")]
+    public object Ping()
+    {
+        return new
+        {
+            success = true,
+            message = "ProfitX Cloud Server is running!",
+            time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            version = "1.0",
+            developer = "London Cyber 2026"
+        };
+    }
+
+    [HttpGet("status")]
+    public object Status()
+    {
+        return new
+        {
+            success = true,
+            serverName = "ProfitX Cloud Server",
+            version = "1.0",
+            uptime = DateTime.Now.ToString(),
+            environment = "Railway Cloud"
+        };
+    }
+}
