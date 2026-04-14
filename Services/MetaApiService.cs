3a848b64e6e1d8d478069f0624034fb23b81b636
London-Cyber020 <londoncyber020@gmail.com>
2026-04-14 03:26:45 +0300

    Delete CandleData class from MetaApiService
    Removed the CandleData class from MetaApiService.

--- Services/MetaApiService.cs
+++ Services/MetaApiService.cs
@@ -475,13 +475,3 @@ public class MetaApiService
         return candles;
     }
 }
-
-public class CandleData
-{
-    public string Time { get; set; } = "";
-    public double Open { get; set; }
-    public double High { get; set; }
-    public double Low { get; set; }
-    public double Close { get; set; }
-    public long Volume { get; set; }
-}
